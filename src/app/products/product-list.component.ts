05e9e1256e333b6e0c1c0f2d664a5fd5c303d987
Vincius Costa <vinicius.costalins@gmail.com>
2019-07-11 14:18:22 -0300

    Criação do product-details e product details guard e configuracao de rotas

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,23 +3,22 @@ import { IProduct } from './product';
 import { ProductService } from './products.service';
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  
+
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    
+
     _listFilter: string = 'cart';
 
     get listFilter(): string {
       return this._listFilter;
     }
-    
+
     set listFilter(value: string){
       this._listFilter = value;
       this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
@@ -29,7 +28,7 @@ export class ProductListComponent implements OnInit {
     products: IProduct[] = [];
 
     constructor(private productService: ProductService) {
-           
+
     }
 
     onRatingClicked(message: string): void {
@@ -38,17 +37,17 @@ export class ProductListComponent implements OnInit {
 
     performFilter(filterBy: string): IProduct[] {
         filterBy = filterBy.toLocaleLowerCase();
-        return this.products.filter((product: IProduct) => 
+        return this.products.filter((product: IProduct) =>
           product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
     }
 
     toggleImage() : void {
       this.showImage = !this.showImage;
-    }    
-    
+    }
+
     ngOnInit(): void {
       this.products = this.productService.getProducts();
-      
+
     }
 
 }
